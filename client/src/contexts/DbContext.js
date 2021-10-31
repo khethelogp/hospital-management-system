673d2f1ac3e45350fd414d20e5fcacba7b5a6956
Khethelogp <khethelogp@gmail.com>
2021-10-31 23:45:27 +0200

    add doctor to db

--- client/src/contexts/DbContext.js
+++ client/src/contexts/DbContext.js
@@ -10,13 +10,22 @@ export function useDB() {
 
 export default function DbProvider({ children }) {
     const [users, setUsers] = useState([]);
+    const [doctors, setDoctors] = useState([]);
+
     const usersCollectionRef = collection(db, "users");
+    const doctorsCollectionRef = collection(db, "doctors");
     
     const fetchUsers = async() => {
         const data = await getDocs(usersCollectionRef);
         setUsers(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
     }
 
+    const fetchDoctors = async() => {
+        const data = await getDocs(doctorsCollectionRef);
+        setDoctors(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
+    }
+
+    // create new collections in the DB
     const createUser = async(fName, lName, mail, contact, userId) => {
         await addDoc(usersCollectionRef, {
             firstName: fName,
@@ -24,21 +33,37 @@ export default function DbProvider({ children }) {
             email: mail,
             phone: contact,
             uid: userId 
-        })   
+        });   
     }
-    
+
+    const createNewDoctor = async(drname, specialty, mail, passcode, roomNr ) => {
+        await addDoc(doctorsCollectionRef, {
+            name: drname,
+            specialization: specialty,
+            email: mail,
+            password: passcode,
+            roomNumber: roomNr
+        });
+    }
+
+
+    // value to return forn useDB();
     const value = {
         users,
+        doctors,
         createUser,
+        createNewDoctor,
     }
 
     useEffect(() => {
         fetchUsers();
-        
+        fetchDoctors();
+
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []) 
 
     console.log(users);
+    console.log(doctors);
 
 
     return (

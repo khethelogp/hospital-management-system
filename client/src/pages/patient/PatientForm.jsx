b9eb51f052c290ffd6ff616f631f7c106a2258fe
Khethelogp <khethelogp@gmail.com>
2021-11-05 21:23:38 +0200

    patient form dateTime-input

--- client/src/pages/patient/PatientForm.jsx
+++ client/src/pages/patient/PatientForm.jsx
@@ -1,10 +1,9 @@
 import React, { useState } from 'react';
-import { Button, CssBaseline, Grid, MenuItem, TextField } from '@mui/material';
+import { Button, Grid, MenuItem, TextField } from '@mui/material';
 import useStyles from './styles'
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
-import AdapterDateFns from '@mui/lab/AdapterDateFns';
-import { LocalizationProvider, DatePicker, TimePicker} from '@mui/lab/';
+import { DatePickerField, TimePickerField, DateTimePickerField } from '../../components';
 import { useDB } from '../../contexts/DbContext';
 
 
@@ -16,28 +15,19 @@ const PatientForm = () => {
         specialization: '',
         doctor: '',
         roomNumber: 1,
-        appointmentDate: new Date(),
-        appointmentTime: new Date(),
-        email: ''
+        appointmentDate: '',
+        appointmentTime: new Date().toLocaleTimeString(),
+        appointmentDateTime: ''
     }
 
     const [room, setRoom] = useState(0);
-    const [dateValue, setDateValue] = useState(null);
-    const [timeValue, setTimeValue] = useState(new Date('2014-08-18T21:11:54'));
 
-    const handleInputChange = (newValue) => {
-        setTimeValue(newValue);
-    };
-    
-    
     const validationSchema = Yup.object().shape({
         specialization: Yup.string().required("Please select a specialization"),
         doctor: Yup.string().required("Please select a doctor"),
         appointmentDate: Yup.date().required('Please choose a date'),
-        appointmentTime: Yup.date().required('Please choose a time'),
-        // email: Yup.string().email('Please enter a valid email').required('Required'),
-        // appointmentTime: Yup.date().default(() => new Date().getTime()).required('Please choose a time'),
-
+        appointmentTime: Yup.date().required('Please choose a time'),   
+        appointmentDateTime: Yup.date().required('Please choose a time'),   
     })
     
 
@@ -133,45 +123,48 @@ const PatientForm = () => {
                             </Grid>
                             
                             <Grid item xs={12} sm={12} md={6} lg={6}>
-                                <LocalizationProvider dateAdapter={AdapterDateFns}>
-                                    <CssBaseline />
-                                    
-                                    <Field
-                                        as={DatePicker}
-                                        name="appointmentDate"
-                                        label="Date"
-                                        id="appointmentDate"
-                                        required
-                                        value={dateValue}
-                                        onChange={(newValue) => {
-                                        setDateValue(newValue);
-                                        }}
-                                        renderInput={(params) => <TextField {...params} helperText={<ErrorMessage name="appointmentDate"/> } />}
-                                    />
-                                    {/* <DatePicker
-                                        name="appointmentDate"
-                                        label="Date"
-                                        id="appointmentDate"
-                                        value={dateValue}
-                                        onChange={(newValue) => {
-                                        setDateValue(newValue);
-                                        }}
-                                        renderInput={(params) => <TextField {...params} helperText={<ErrorMessage name="appointmentDate"/>} />}
-                                    /> */}
-                                    <TimePicker
-                                        name="appointmentTime"
-                                        label="Time"
-                                        id="appointmentTime"
-                                        value={timeValue}
-                                        onChange={handleInputChange}
-                                        renderInput={(params) => <TextField {...params} helperText={<ErrorMessage name="appointmentTime"/>} />}
-                                    />
-                                </LocalizationProvider>
+                                <Field 
+                                    component={DatePickerField}
+                                    name="appointmentDate" 
+                                    // label="Date"
+                                    id="appointmentDate"
+                                    variant="inline"
+                                    // variant="outlined"
+                                    inputVariant="outlined" 
+                                    // helperText={<ErrorMessage name="appointmentDate"/>}
+                                    renderInput={(params) => <TextField {...params} helperText={<ErrorMessage name="appointmentTime"/> } />}
+                                />
+
+                                <Field 
+                                    component={TimePickerField}
+                                    name="appointmentTime" 
+                                    id="appointmentTime"
+                                    variant="inline"
+                                    // variant="outlined"
+                                    inputVariant="outlined"
+                                    placeholder="08:00 AM" 
+                                    helperText={<ErrorMessage name="appointmentTime"/>}
+                                />
+
+                                <Field 
+                                    component={DateTimePickerField}
+                                    name="appointmentDateTime" 
+                                    id="appointmentDateTime"
+                                    // variant="inline"
+                                    variant="outlined"
+                                    inputVariant="outlined"
+                                    helperText={<ErrorMessage name="appointmentTime"/>}
+                                />
+
+                                
+
+                                
                             </Grid>
 
                             <Grid item xs={12} sm={12} md={6} lg={6}>
                                 <Button
                                     variant="contained"
+                                    type="submit"
                                     color="primary"
                                     size="large"
                                     sx={{ m: 2 }}
@@ -187,3 +180,48 @@ const PatientForm = () => {
 }
 
 export default PatientForm
+
+
+/* 
+<LocalizationProvider dateAdapter={AdapterDateFns}>
+                                    <CssBaseline />
+                                    
+                                    
+
+                                    <Field
+                                        as={TimePicker}
+                                        name="appointmentTime"
+                                        label="Time"
+                                        id="appointmentTime"
+                                        required
+                                        value={timeValue}
+                                        onChange={(newValue) => {
+                                            setDateValue(newValue);
+                                            set
+                                        }}
+                                        renderInput={(params) => <TextField {...params} helperText={<ErrorMessage name="appointmentTime"/> } />}
+                                    /> 
+
+                                    <TimePicker
+                                        name="appointmentTime"
+                                        label="Time"
+                                        id="appointmentTime"
+                                        value={timeValue}
+                                        onChange={handleInputChange}
+                                        renderInput={(params) => <TextField {...params} helperText={<ErrorMessage name="appointmentTime"/>} />}
+                                    />
+                                    
+                                    <DatePicker
+                                        name="appointmentDate"
+                                        label="Date"
+                                        id="appointmentDate"
+                                        value={dateValue}
+                                        onChange={(newValue) => {
+                                        setDateValue(newValue);
+                                        }}
+                                        renderInput={(params) => <TextField {...params} helperText={<ErrorMessage name="appointmentDate"/>} />}
+                                    />
+                                </LocalizationProvider>
+
+
+*/
